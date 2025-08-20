d088732dc2ce04c4ce5ebac3e0cd2c943a706777
Beatriz Berten <271332@unifio.edu.br>
2025-08-20 01:39:44 +0000

    codigos-de-alta-performance

--- src/main/java/app/PrimeiraClasse.java
+++ src/main/java/app/PrimeiraClasse.java
@@ -3,5 +3,12 @@ package app;
 public class PrimeiraClasse {
     public static void main(String[] args) {
         System.out.println("Olá Gradle!!!");
+
+        Aluno al = new Aluno();
+        al.setId(1);
+        al.setNome("Perereco");
+        al.setIdade(141);
+
+        System.out.println(al.getNome());
     }
 }
