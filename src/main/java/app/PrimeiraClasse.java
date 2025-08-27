0366e452672c3b9bde0cf98cdf0b86b45e867153
Beatriz Berten <271332@unifio.edu.br>
2025-08-27 01:41:04 +0000

    aula_26/08/2025

--- src/main/java/app/PrimeiraClasse.java
+++ src/main/java/app/PrimeiraClasse.java
@@ -1,5 +1,8 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeiraClasse {
     public static void main(String[] args) {
         System.out.println("Olá Gradle!!!");
@@ -10,5 +13,24 @@ public class PrimeiraClasse {
         al.setIdade(141);
 
         System.out.println(al.getNome());
+
+
+
+        Tarefa t1 = new Tarefa();
+        t1.setDescricao("Estudar Estrutura de Dados");
+        t1.setConcluido(true);
+
+        Tarefa t2 = t1;
+        t2.setDescricao("Exemplo de tarefa");
+
+        System.out.println(t1.getDescricao());
+        System.out.println(t2.getDescricao());
+
+        List <Atividade> agenda = new ArrayList<Atividade>();
+        agenda.add(t1);
+        Compromisso c1 = new Compromisso();
+        agenda.add(c1);
+
+        System.out.println(t1);
     }
 }
